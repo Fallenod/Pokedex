3b00ab6312265c73b15b09793d6f509626f30b5f
Fallenod <thefallenod@gmail.com>
2021-10-28 21:14:26 +0500

    Updates

---
+++ static/js/3.2306572e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonppokedex=this.webpackJsonppokedex||[]).push([[3],{38:function(e,s,t){"use strict";t.r(s);var c=t(0);s.default=function(e){var s=e.srcImg;return Object(c.jsx)("img",{src:s,alt:""})}}}]);
+//# sourceMappingURL=3.2306572e.chunk.js.map
